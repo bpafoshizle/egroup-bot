c83209148151a183e6e4968ec784d9a07a0a8d95
bpafoshizle <bpafoshizle@gmail.com>
2022-05-07 12:19:17 -0500

    fixed black and flake8 lint

--- app/app.py
+++ app/app.py
@@ -1,10 +1,8 @@
 import logging
-import os
 
-from discord.ext import commands
 from pydiscogs import botbuilder
 
-bot = botbuilder.build_bot('egroup-bot.yaml')
+bot = botbuilder.build_bot("egroup-bot.yaml")
 
 logging.info("running bot: %s", bot)
 bot.run(bot.discord_token)
