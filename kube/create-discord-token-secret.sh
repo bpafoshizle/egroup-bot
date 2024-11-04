68a13b484fd9e8004c37ef9181b63b3061ee5870
bpafoshizle <bpafoshizle@gmail.com>
2024-11-03 21:07:40 -0600

    executable sh files

--- kube/create-discord-token-secret.sh
+++ kube/create-discord-token-secret.sh
@@ -11,6 +11,5 @@ kubectl create secret generic discord \
     --from-literal=dscrd-chnl-gaming="${DSCRD_PROD_CHNL_GAMING}" \
     --from-literal=dscrd-chnl-bot-feature-requests="${DSCRD_PROD_CHNL_BOT_FEATURE_REQUESTS}" \
     --from-literal=dscrd-guild-id="${DISCORD_EGROUP_GUILD_ID}" \
-    --dry-run=client -o yaml \
-    | kubectl --kubeconfig ~/.kube/bletchley-config -n egroup-bot apply -f -
-
+    --dry-run=client -o yaml |
+    kubectl --kubeconfig ~/.kube/bletchley-config -n egroup-bot apply -f -
--- kube/create-twitch-secret.sh
+++ kube/create-twitch-secret.sh
@@ -5,5 +5,6 @@ kubectl create secret generic twitch \
     --from-literal=twitch-bot-client-id="${TWITCH_BOT_CLIENT_ID}" \
     --from-literal=twitch-bot-client-secret="${TWITCH_BOT_CLIENT_SECRET}" \
     --from-literal=twitch-chat-oauth-token="${TWITCH_CHAT_OAUTH_TOKEN}" \
-    --dry-run=client -o yaml \
-    | kubectl --kubeconfig ~/.kube/bletchley-config -n egroup-bot apply -f -
+    --dry-run=client -o yaml | 
+    kubectl --kubeconfig ~/.kube/bletchley-config -n egroup-bot apply -f -
+
